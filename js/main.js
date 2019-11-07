22977e5230b520e80396f39f9bd7989322327b27
ArunkmrS <Arunkumars@live.in>
2019-11-07 22:56:08 +0530

    Adde map module

--- js/main.js
+++ js/main.js
@@ -205,3 +205,21 @@
 
 })(jQuery);
 
+function initMap() {
+  // The location of Uluru
+  var uluru = {
+    lat: 23.810597,
+    lng: 86.452084
+  };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 16,
+      center: uluru
+    });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
